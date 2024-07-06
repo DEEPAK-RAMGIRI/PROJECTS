b4add3af0b2fb7117ba3f19be761435926eb3fef
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2024-07-06 15:39:04 +0530

    country capitls flag quizz

--- GAMES/QUIZ capitals/flags/country.js
+++ GAMES/QUIZ capitals/flags/country.js
@@ -1,6 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
+const db = new pg.Client({
+  user:"postgres",
+  host:"localhost",
+  database:"world",
+  password:"270104",
+  port:5432,
+});
+db.connect();
+let quiz=[];
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 const app = express();
 const port = 3000;
 
@@ -17,21 +35,21 @@ app.get("/", (req, res) => {
   totalCorrect = 0;
   nextQuestion();
   console.log(currentQuestion);
-  res.render("index.ejs", { question: currentQuestion });
+  res.render("country.ejs", { question: currentQuestion });
 });
 
 // POST a new post
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
   }
 
   nextQuestion();
-  res.render("index.ejs", {
+  res.render("country.ejs", {
     question: currentQuestion,
     wasCorrect: isCorrect,
     totalScore: totalCorrect,
